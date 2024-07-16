6955d671126350b5b1f2a10c09a8ccc6044eef12
sraj69778 <sraj69778@gmail.com>
2024-07-17 00:41:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/sumit', (req, res) => {
+    res.send('Hello Sumit!')
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
